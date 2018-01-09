a0e8ed631ebd8dcd68342962384411f71632a3f7
sanjiv sahayam <sanjsmailbox@gmail.com>
2018-01-09 23:43:50 +1100

    updated to match gist

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 -- import Lib
 import Text.Printf (printf)
+import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Writer.Lazy
@@ -20,20 +21,39 @@ kim = Person "Kim Lynch"
 peopleDb :: [Person]
 peopleDb = [alex, philip, kim]
 
-process :: ReaderT Person (WriterT String IO) String
-process = do
-  _ <- lift . tell $ "Looking up Person"
+process' :: ReaderT Person (WriterT String IO) String
+process' = do
+  _ <- lift . tell $ "Looking up Person... "
   Person p <- ask
-  _ <- lift . tell $ "Found person " ++ p
+  _ <- lift . tell $ printf "Found person: %s. " p
   return p
 
-main :: IO ()
-main = runProcess
+process :: ReaderT Person (WriterT String IO) ()
+process = do
+  _ <- lift . tell $ "Looking up Person. "
+  Person p <- ask
+  _ <- lift . tell $ printf "Found person: %s. " p
+  (liftIO . putStrLn) p
 
-runProcess :: IO ()
-runProcess =
-  let processed = mapM (runReaderT process) peopleDb
-      written   = runWriterT processed
-  in do (result, log) <- written
-        _ <- putStrLn $ printf "result: %s" (show result)
-        putStrLn $ printf "log: %s" log
+
+main :: IO ()
+main = do
+  -- Print name from monad transformer
+  result1 <- runWriterT (runReaderT process alex) -- :: ((), String)
+  _ <- (putStrLn . snd) result1
+
+  -- Extract the name from monad transfomer, then print it
+  result2 <- runWriterT (runReaderT process' alex) -- :: (String, String)
+  _ <- (putStrLn . fst) result2
+  _ <- (putStrLn . snd) result2
+
+  -- Now do the same thing for a list of people using mapM
+  result3 <- runWriterT (mapM (runReaderT process') peopleDb) -- :: ([String], String)
+
+  let people = fst result3
+      log    = snd result3
+
+  _ <- putStrLn "\n\nReaderT values:\n"
+  _ <- mapM_ putStrLn people
+  _ <- putStrLn "\nWriterT log:\n"
+  putStrLn log
